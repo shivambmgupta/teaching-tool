a7b910e2a85835659e3344b71652024c885226b1
shivamgmgupta <shivamgmgupta@gmail.com>
2020-06-12 23:09:59 +0530

    Temp splash screen added.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTimer>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QPixmap pixmap(":/icons/icons/splashScreen.png");
+    QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
+    splash.show();
+
     MainWindow w;
-    w.show();
+
+    QTimer::singleShot(2000, &splash, SLOT(close()));
+    QTimer::singleShot(2000, &w, SLOT(showMaximized()));
+
     return a.exec();
 }
